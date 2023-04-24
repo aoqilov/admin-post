19ab44e84c162a096b4674cc0f7f125c5fb14c3a
Abdulaziz <aoqilov88@gmail.com>
2023-04-24 23:05:03 +0500

    ytre

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import jsconfigPaths from "vite-jsconfig-paths";
 import sass from "sass";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/admin-post",
   plugins: [react(), jsconfigPaths()],
   css: {
     preprocessorOptions: {
